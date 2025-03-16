1aa11fb495195a20e0cfd78edc8622004c4e1f56
sazzadabdulhasib <sazzad@getdrop.com>
2025-03-16 23:19:13 +0000

    Updates

---
+++ precache-manifest.d20ae5c491fece7f4f825b2f0f6110bf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7f1cefb449ab6c378817f33cc0458df",
+    "url": "/imcc/index.html"
+  },
+  {
+    "revision": "1a40bc6956b961ba52d5",
+    "url": "/imcc/static/css/main.d5eec487.chunk.css"
+  },
+  {
+    "revision": "1cfcc39763c72766a54e",
+    "url": "/imcc/static/js/2.dd8c59a2.chunk.js"
+  },
+  {
+    "revision": "fc0ba07c0f771d66c44444381376eaf8",
+    "url": "/imcc/static/js/2.dd8c59a2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f105109532f51ba40994",
+    "url": "/imcc/static/js/3.6fbdc85c.chunk.js"
+  },
+  {
+    "revision": "1a40bc6956b961ba52d5",
+    "url": "/imcc/static/js/main.ba03e703.chunk.js"
+  },
+  {
+    "revision": "5242ce8905cfe9214fb0",
+    "url": "/imcc/static/js/runtime-main.c9ae3f02.js"
+  }
+]);
